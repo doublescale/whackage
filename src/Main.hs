8918c37dc5f527784c1ca32c799e9f2432185ce3
doublescale <doublescale@tutanota.com>
2017-09-20 22:07:40 +0200

    set up minimal Brick app

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+import Control.Monad
+
+import Brick
+import Brick.Widgets.Center
+import Graphics.Vty
+
 main :: IO ()
-main = putStrLn "Whack!"
+main = void $ customMain (mkVty defaultConfig) Nothing myApp initState
+
+type AppState = ()
+type AppEvent = ()
+type AppName = ()
+type MyApp = App AppState AppEvent AppName
+
+myApp :: MyApp
+myApp = App
+  { appDraw = renderState
+  , appChooseCursor = showFirstCursor
+  , appHandleEvent = eventHandler
+  , appStartEvent = return
+  , appAttrMap = \s -> attrMap defAttr []
+  }
+
+initState :: AppState
+initState = ()
+
+renderState :: AppState -> [Widget AppName]
+renderState state = pure . center . str $ "Whack!"
+
+eventHandler :: AppState
+             -> BrickEvent AppName AppEvent
+             -> EventM AppName (Next AppState)
+eventHandler state (VtyEvent e) | isQuitEvent e = halt state
+eventHandler state (AppEvent e) = continue e
+eventHandler state _ = continue $ state
+
+isQuitEvent :: Event -> Bool
+isQuitEvent (EvKey KEsc _) = True
+isQuitEvent _ = False
