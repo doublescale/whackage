364d3c85c56ff21030d49c78ae84917f004f716d
doublescale <doublescale@tutanota.com>
2017-09-20 23:16:02 +0200

    define game grid

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad
+import qualified Data.Vector as Vector
 
 import Brick
 import Brick.Widgets.Center
@@ -9,7 +10,10 @@ import Graphics.Vty
 main :: IO ()
 main = void $ customMain (mkVty defaultConfig) Nothing myApp initState
 
-type AppState = ()
+data AppState = AppState
+  { gameGrid :: Vector.Vector Target
+  }
+data Target = NoTarget | Enemy
 type AppEvent = ()
 type AppName = ()
 type MyApp = App AppState AppEvent AppName
@@ -24,18 +28,38 @@ myApp = App
   }
 
 initState :: AppState
-initState = ()
+initState = AppState
+  { gameGrid = Vector.replicate 9 Enemy
+  }
 
 renderState :: AppState -> [Widget AppName]
-renderState state = pure . center . str $ "Whack!"
+renderState state = pure . center . renderGrid . gameGrid $ state
+  where
+    renderGrid grid =
+      vBox
+        [ hBox
+          [ renderTarget $ grid Vector.! (x + 3*y)
+          | x <- [0,1,2] ]
+        | y <- [2,1,0] ]
+    renderTarget NoTarget = str ". "
+    renderTarget Enemy    = str "X "
 
 eventHandler :: AppState
              -> BrickEvent AppName AppEvent
              -> EventM AppName (Next AppState)
-eventHandler state (VtyEvent e) | isQuitEvent e = halt state
-eventHandler state (AppEvent e) = continue e
-eventHandler state _ = continue $ state
-
-isQuitEvent :: Event -> Bool
-isQuitEvent (EvKey KEsc _) = True
-isQuitEvent _ = False
+eventHandler state (VtyEvent (EvKey k _)) = handleKey k
+  where
+    handleKey KEsc        = halt state
+    handleKey (KChar '1') = hitTarget 0
+    handleKey (KChar '2') = hitTarget 1
+    handleKey (KChar '3') = hitTarget 2
+    handleKey (KChar '4') = hitTarget 3
+    handleKey (KChar '5') = hitTarget 4
+    handleKey (KChar '6') = hitTarget 5
+    handleKey (KChar '7') = hitTarget 6
+    handleKey (KChar '8') = hitTarget 7
+    handleKey (KChar '9') = hitTarget 8
+    handleKey _           = continue state
+    hitTarget i =
+      continue $ state { gameGrid = gameGrid state Vector.// [(i, NoTarget)] }
+eventHandler state _ = continue state
