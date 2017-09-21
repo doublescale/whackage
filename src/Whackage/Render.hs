f051caa122c2eab3c7d6f3949a04793b71590ce2
doublescale <doublescale@tutanota.com>
2017-09-21 07:30:31 +0200

    better enemy graphics

--- src/Whackage/Render.hs
+++ src/Whackage/Render.hs
@@ -17,5 +17,5 @@ renderState state = pure . center . renderGrid . gameGrid $ state
           [ renderTarget $ grid ! (x + 3*y)
           | x <- [0,1,2] ]
         | y <- [2,1,0] ]
-    renderTarget NoTarget = str ". "
-    renderTarget Enemy    = str "X "
+    renderTarget NoTarget = str "... " <=> str "    "
+    renderTarget Enemy    = str "òuó " <=> str "    "
